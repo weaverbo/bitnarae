85af229f68dfc5381fc28c54e1d576375f2e9241
kyeongbobak <kyeong0914@gmail.com>
2025-07-15 11:57:56 +0900

    feat : prisma 클라이언트 설치 후 API 라우트 구현하고, 공지사항 데이터 불러오기 완료

--- src/components/NoticeSection.tsx
+++ src/components/NoticeSection.tsx
@@ -1,5 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function NoticeSection() {
-  // 데이터베이스 만들기
+  type Notice = {
+    id: number;
+    title: string;
+    created_at: string;
+  };
+
+  const [notices, setNotices] = useState<Notice[]>([]);
+
+  useEffect(() => {
+    const noticeList = async (): Promise<void> => {
+      const res = await fetch("/api/notice");
+      const data = await res.json();
+      console.log(data, "여기 데이터에요!");
+      return setNotices(data);
+    };
+    noticeList();
+  }, []);
+
   return (
     <>
       <div className="container flex gap-[320px] justify-between">
@@ -9,12 +30,16 @@ export default function NoticeSection() {
             <h2 className="text-[48px] font-semibold">공지사항</h2>
           </div>
         </div>
-        <div className="border-t-2 border-black w-[930px] flex justify-between px-[80px] py-[32px] text-[24px]">
-          <div className="flex gap-[24px]">
-            <span>1</span>
-            <p>2024 빛나래 문화재단 후원금 사용 명세</p>
-          </div>
-          <p>2024-12-31</p>
+        <div>
+          {notices.map((notice, index) => (
+            <div key={index} className="border-t-2 border-black w-[930px] flex justify-between px-[80px] py-[32px] text-[24px]">
+              <div className="flex gap-[24px]">
+                <span>{notice.id}</span>
+                <p>{notice.title}</p>
+              </div>
+              <p>{notice.created_at}</p>
+            </div>
+          ))}
         </div>
       </div>
     </>
