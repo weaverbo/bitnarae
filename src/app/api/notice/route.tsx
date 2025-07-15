af5fbf2f5b714f78bddb6368e699967eebdd7f7a
kyeongbobak <kyeong0914@gmail.com>
2025-07-15 18:42:01 +0900

    catch 블록의 unused error 변수 제거

--- src/app/api/notice/route.tsx
+++ src/app/api/notice/route.tsx
@@ -5,7 +5,7 @@ export async function GET() {
   try {
     const data = await prisma.notice.findMany();
     return NextResponse.json(data, { status: 200 });
-  } catch (error) {
+  } catch {
     return NextResponse.json({ error: "Error fetching data" }, { status: 500 });
   }
 }
