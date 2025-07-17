43201123e9795822716d042f1019e9cacc81005a
kyeongbobak <kyeong0914@gmail.com>
2025-07-17 18:47:27 +0900

    style : slideleftfade 스타일 속성 변경

--- src/components/ui/SlideLeftFade.tsx
+++ src/components/ui/SlideLeftFade.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 export default function SlideLeftFade({ children, className = "" }: { children: React.ReactNode; className?: string }) {
   return (
     <>
-      <div className="overflow-hidden relative" style={{ width: "fit-content" }}>
+      <div className="overflow-hidden">
         <motion.div className={className} initial={{ x: -520, opacity: 1 }} animate={{ x: 0 }} transition={{ duration: 0.6, ease: "easeOut" }}>
           {children}
         </motion.div>
