2fd2d7300c13a53c38b43ae7a5604b9cc18866c3
kyeongbobak <kyeong0914@gmail.com>
2025-07-18 10:35:38 +0900

    feat : 헤더 진입 시 인터랙션 감지하도록 훅 적용, 메뉴에 hover 인터랙션 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,34 +2,47 @@
 
 import StyledLink from "next/link";
 import SlideLeftFade from "../components/ui/SlideLeftFade";
+import { useAnimateOnInView } from "../hook/useAnimateOnInView";
+import "../styles/header.css";
 
-// 여기 애니메이션이 사용자가 화면 맨 끝으로 갔다가 Header가 화면에 보였을 때 애니메이션을 재 실행되게 할것인지?
 export default function Header() {
+  const { onInViewRef, animationKey } = useAnimateOnInView();
+
   return (
     <div className="position absolute z-10 bg-white/10 w-full">
       <div className="flex items-center justify-between container py-[64px]">
         <div className="flex text-white items-center gap-[8px]">
           <h1 className="text-2xl font-semibold">빛나래문화재단</h1>
-          <SlideLeftFade className="flex items-center gap-[8px]">
-            <div className="w-[2px] h-[38px] bg-white"></div>
-            <div className="text-[16px] font-semibold leading-[19px]">
-              <p>BITNARAE</p>
-              <p>FOUNDATION OF CULTURE</p>
-            </div>
-          </SlideLeftFade>
+          <div ref={onInViewRef}>
+            <SlideLeftFade className="flex items-center gap-[8px]" animationKey={animationKey}>
+              <div className="w-[2px] h-[38px] bg-white"></div>
+              <div className="text-[16px] font-semibold leading-[19px]">
+                <p>BITNARAE</p>
+                <p>FOUNDATION OF CULTURE</p>
+              </div>
+            </SlideLeftFade>
+          </div>
         </div>
         <ul className="flex gap-[24px] text-lg text-white">
           <li>
-            <StyledLink href={"/"}>재단소개</StyledLink>
+            <StyledLink className="header-menu-link" href={"/"}>
+              재단소개
+            </StyledLink>
           </li>
           <li>
-            <StyledLink href={"/"}>재단활동</StyledLink>
+            <StyledLink className="header-menu-link" href={"/"}>
+              재단활동
+            </StyledLink>
           </li>
           <li>
-            <StyledLink href={"/"}>재단소식</StyledLink>
+            <StyledLink className="header-menu-link" href={"/"}>
+              재단소식
+            </StyledLink>
           </li>
           <li>
-            <StyledLink href={"/"}>공지사항</StyledLink>
+            <StyledLink className="header-menu-link" href={"/"}>
+              공지사항
+            </StyledLink>
           </li>
         </ul>
       </div>
