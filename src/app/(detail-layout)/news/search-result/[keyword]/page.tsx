5fabbf9daba38a341e2452a9bcf98b10e1e241f8
kyeongbobak <kyeong0914@gmail.com>
2025-08-21 13:27:26 +0900

    fix : page 컴포넌트 props 타입 수정: searchParams 타입 정의 추가

--- src/app/(detail-layout)/news/search-result/[keyword]/page.tsx
+++ src/app/(detail-layout)/news/search-result/[keyword]/page.tsx
@@ -3,7 +3,9 @@ import InfoBoardTemplate from "@/components/common/InfoBoardTemplate";
 
 type Params = { keyword: string };
 
-export default async function NewsSearchResult({ params }: { params: Params }) {
+type SearchParams = { [key: string]: string | string[] | undefined };
+
+export default async function NewsSearchResult({ params, searchParams }: { params: Params; searchParams?: SearchParams }) {
   const keyword = decodeURIComponent(params.keyword ?? "").trim();
 
   const data = await prisma.news.findMany({
